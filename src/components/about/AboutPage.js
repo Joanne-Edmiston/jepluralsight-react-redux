eb27e3feb55404ad59f12806e5659399e03b6ab5
Joanne Edmiston <joanneedmiston@grofar.com>
2018-07-01 17:42:57 +0100

    tutorial - inital checkin

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React Router and other shiz</p>
+            </div>
+        );
+    }
+
+}
+
+export default AboutPage;
