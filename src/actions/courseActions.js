86cc3c5add5cd477bf4833461cd1569dc3c86d13
Joanne Edmiston <joanneedmiston@grofar.com>
2018-07-08 17:04:45 +0100

    show loader when ajax calls outstanding

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes.js';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses){
     return {type: types.LOAD_COURSES_SUCCESS, courses};
@@ -17,6 +18,8 @@ export function loadCourses(){
 
     return function(dispatch){
 
+        dispatch(beginAjaxCall());
+
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
@@ -30,6 +33,8 @@ export function loadCourses(){
 export function saveCourse(course){
     return function (dispatch, getState){
         
+        dispatch(beginAjaxCall());
+
         return courseApi.saveCourse(course).then(savedCourse => {
             course.id 
                 ? dispatch(updateCourseSuccess(savedCourse))
